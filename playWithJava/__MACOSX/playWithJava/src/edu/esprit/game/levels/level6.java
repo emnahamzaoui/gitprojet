7eadbcf123889d7342fe15a3854e599e3f12840e
Acer <zeinbchebbi44@gmail.com>
2023-01-30 22:08:16 +0100

    v4

--- playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level6.java
+++ playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level6.java
@@ -1,4 +1,4 @@
-public class level5 {
+public class level6 {
     public static void main(String args[])  {
     System.out.println ("level6"); }
   
---
+++ playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level7.java
@@ -0,0 +1,5 @@
+public class level7 {
+    public static void main(String args[])  {
+        System.out.println ("Hello hiho"); }
+      
+}
