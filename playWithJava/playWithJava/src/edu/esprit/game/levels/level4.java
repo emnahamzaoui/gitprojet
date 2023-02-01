70a949de21c6f7c0c20c218d4d36b3af059f7b28
Da7labou123 <anas.chaari22@gmail.com>
2023-02-01 15:43:58 +0100

    second update

--- playWithJava/playWithJava/src/edu/esprit/game/levels/level4.java
+++ playWithJava/playWithJava/src/edu/esprit/game/levels/level4.java
@@ -4,5 +4,7 @@ package edu.esprit.game.levels;
 public class level4{
     public static void main(String[] args){
     System.out.println("riadh");
+    System.out.println("riadh12222");
+
     }
 }
