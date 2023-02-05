ea32a56c8bb7c7e500c6bbaf9c8ea43f9926d562
Da7labou123 <anas.chaari22@gmail.com>
2023-02-05 15:07:31 +0100

    resolved conflict v01

--- playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level9.java
+++ playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level9.java
@@ -2,9 +2,6 @@ public class level9 {
     
     
     public static void main(String args[])  {
-<<<<<<< HEAD
+
         System.out.println ("level 9998"); }
-=======
-      
->>>>>>> 56e4d6b50d160316a3a5d345245dc51dc2814a66
 }
