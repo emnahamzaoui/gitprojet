9a7df54b5d1b0a1219a8d96319e681649f183502
Da7labou123 <anas.chaari22@gmail.com>
2023-02-05 22:58:36 +0100

    test1512

--- playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level9.java
+++ playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level9.java
@@ -3,7 +3,9 @@ public class level9 {
     
     public static void main(String args[])  {
         System.out.println ("level 9889"); 
+        System.out.println ("level 9810");
         System.out.println ("level 9810"); 
+ 
 
         }
 }
