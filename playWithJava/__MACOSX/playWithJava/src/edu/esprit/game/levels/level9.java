150fac815abc28bcae0d879bf4da5ea6b037c05f
Da7labou123 <anas.chaari22@gmail.com>
2023-02-05 22:33:07 +0100

    confict test

--- playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level9.java
+++ playWithJava/__MACOSX/playWithJava/src/edu/esprit/game/levels/level9.java
@@ -3,5 +3,8 @@ public class level9 {
     
     public static void main(String args[])  {
 
-        System.out.println ("level 9998"); }
+        System.out.println ("level 9998");
+        System.out.println ("level 100000");
+
+         }
 }
