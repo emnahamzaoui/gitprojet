db67412f31800fa5094ed7ec4e813f03eea6c850
oumaima <oumaimachaouali5@gmail.com>
2023-02-08 13:36:28 +0100

    hello

--- playWithJava/playWithJava/src/edu/esprit/game/levels/level4.java
+++ playWithJava/playWithJava/src/edu/esprit/game/levels/level4.java
@@ -5,6 +5,7 @@ public class level4{
     public static void main(String[] args){
     System.out.println("riadh");
     System.out.println("riadh12222");
-    System.out.println("oumaaa12222");
+    System.out.println("oumaaa012222");
+    System.out.println("hello F");
     }
 }
